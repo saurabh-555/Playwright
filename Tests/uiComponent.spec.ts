690a11bbf7cd6ff4b7f72cdd5c09f59461367829
saurabh-555 <saurabh.hazaribag555@gmail.com>
2026-05-31 22:59:04 +0530

    ui element complete

--- Tests/uiComponent.spec.ts
+++ Tests/uiComponent.spec.ts
@@ -90,12 +90,100 @@ test ("tooltip",async({page})=>{
 test("dialogue-box",async({page})=>{
     await page.getByText("Tables & data").click()
     await page.getByText("smart table").click()
-
+    
     page.on('dialog',dialog=>{
         expect(dialog.message()).toEqual;("Are you sure you want to delete?")
         dialog.accept()
     })
     await page.getByRole("table").locator('tr',{hasText:"mdo@gmail.com"}).locator('.nb-trash').click()
-
+    
     await expect(page.locator("table tr").first()).not.toHaveText("mdo@gmail.com")
+})
+test('Web Tables',async({page})=>{
+    await page.getByText("Tables & data").click()
+    await page.getByText("smart table").click()
+
+    //get the target row
+    const targetRow=page.getByRole("row",{name:'twitter@outlook.com'})
+    await targetRow.locator('.nb-edit').click()
+
+    await page.locator('input-editor').getByPlaceholder("Age").clear()
+    await page.locator('input-editor').getByPlaceholder("Age").fill("25")
+    await page.locator('.nb-checkmark').click()
+    
+    //get the row based on the value in the specific column
+    await page.locator('.ng2-smart-pagination').getByText('2').click()
+    const targetRowByID=page.getByRole("row",{name:'11'}).filter({has:page.locator('td').nth(1).getByText("11")})
+    await targetRowByID.locator('.nb-edit').click()
+    await page.locator('input-editor').getByPlaceholder("E-mail").clear()
+    await page.locator('input-editor').getByPlaceholder("E-mail").fill("Test@test.com")
+    await page.locator('.nb-checkmark').click()
+    
+    await expect(targetRowByID.locator('td').nth(5)).toHaveText("Test@test.com")
+    
+    // test filter of the table
+    const ages =  ["20", "30", "40", "200"]
+    
+    for (const age of ages){
+        await page.locator('input-filter').getByPlaceholder("Age").clear()
+        await page.locator('input-filter').getByPlaceholder("Age").fill(age)
+        await page.waitForTimeout(500)
+        const rows= page.locator('tbody tr')
+
+        for (let row of await rows.all()){
+            const ageValue= await row.locator('td').last().textContent()
+            if (age === '200'){
+                expect(await page.getByRole('table').textContent()).toContain('No data found')
+            }
+            else{
+                expect(ageValue).toEqual(age)
+            }
+        }
+    }
+})
+
+test('date-picker',async({page})=>{
+    await page.getByText("Forms").click()
+    await page.getByText("Datepicker").click()
+
+    const calenderDate = page.getByPlaceholder("Form Picker")
+    await calenderDate.click()
+
+    let date = new Date()
+    date.setDate(date.getDate() -1)
+    const expectedDate = date.getDate().toString()
+    const expectedmonth = date.toLocaleString('default',{month:'short'})
+    const expectedYear = date.getFullYear().toString()  
+    const dateToAssert = expectedmonth + " " + expectedDate + ", " + expectedYear
+
+    await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate,{exact:true}).click()
+    await expect(calenderDate).toHaveValue(dateToAssert)
+
+})
+
+test('sliders',async({page})=>{
+
+    //update attribute
+
+    // const tempGauge=page.locator('[tabtitle="Temperature"] ngx-temperature-dragger circle')
+    // await tempGauge.evaluate(node=>{
+    //     node.setAttribute("cx","232.630")
+    //     node.setAttribute("cy","232.630")
+    // })
+    // await tempGauge.click()
+    
+    //mouse movement
+    const tempBox=page.locator('[tabtitle="Temperature"] ngx-temperature-dragger ')
+    await tempBox.scrollIntoViewIfNeeded()
+
+    const box = await tempBox.boundingBox()
+    const x = box.x + box.width / 2
+    const y = box.y + box.height / 2
+    await page.mouse.move(x,y)  
+    await page.mouse.down() 
+    await page.mouse.move(x+100,y)
+    await page.mouse.move(x+100,y+100)
+    await page.mouse.up()
+
+    await expect(tempBox).toContainText("30")
 })
